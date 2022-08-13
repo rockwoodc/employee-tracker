057e1a3054d2c3bec321c419e79ac9f601baec17
rockwoodc <rockwoodce@gmail.com>
2022-08-13 11:58:50 -0400

    fixed tables and checked them

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS department;
--- DROP TABLE IF EXISTS roles;
--- DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -11,15 +11,20 @@ CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2),
-  FOREIGN KEY (department_name) REFERENCES department(department_name)
+  deptId INTEGER NOT NULL,
+  CONSTRAINT fk_deptId FOREIGN KEY (deptId) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  FOREIGN KEY (title) REFERENCES role(title),
-  -- reference another employee that is the manager of the current employee
-  FOREIGN KEY (roles) REFERENCES roles(title) ON DELETE SET NULL
+  employeeRole INTEGER NOT NULL,
+  manager INTEGER,
+  CONSTRAINT fk_employeeRole FOREIGN KEY (employeeRole)
+  REFERENCES roles(id),
+  KEY manager (manager),
+  CONSTRAINT fk_manager FOREIGN KEY (manager)
+  REFERENCES employee (id) ON DELETE SET NULL
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,24 +5,24 @@ VALUES
 ('Finance'),
 ('Legal');
 
-INSERT INTO roles (title, salary)
+INSERT INTO roles (title, salary, deptId)
 VALUES
-('Sales Lead', 100000),
-('Sales person', 80000),
-('Lead Engineer', 150000),
-('Software Engineer', 150000),
-('Accountant', 95000),
-('Account Manager', 90000),
-('Lawyer', 200000),
-('Legal Team Lead', 90000);
+('Sales Lead', 100000, 1),
+('Sales person', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 150000, 2),
+('Accountant', 95000, 3),
+('Account Manager', 90000, 3),
+('Lawyer', 200000, 4),
+('Legal Team Lead', 90000, 4);
 
-INSERT INTO employee (first_name, last_name)
+INSERT INTO employee (first_name, last_name, employeeRole, manager)
 VALUES
-('Josh', 'Hall'),
-('Andy','Waine'),
-('Ryan','Gendel'),
-('Runa','Gendel'),
-('Cortland','Finch'),
-('James','Fraser'),
-('Christian','Rockwood'),
-('Victoria','Nahley');
+('Josh', 'Hall', 1, NULL),
+('Andy','Waine', 1, NULL),
+('Ryan','Gendel', 2, 1),
+('Runa','Gendel', 2, 1),
+('Cortland','Finch', 3, 2),
+('James','Fraser', 3, 2),
+('Christian','Rockwood', 4, 1),
+('Victoria','Nahley', 4, 2);
