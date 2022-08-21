be75cd813aba9b66c022dfb51a0c7857b803f635
rockwoodc <rockwoodce@gmail.com>
2022-08-21 19:47:44 -0400

    added constructors for menu

--- db/index.js
+++ db/index.js
@@ -14,23 +14,23 @@ class Query {
     }
 
     findEmployees() {
-        return this.db.promise().query('')
+        return this.db.promise().query('SELECT employee.name, FROM emplpoyee LEFT JOIN role ON employee.role_id = role.id')
     }
 
     createDepartment() {
-        return this.db.promise().query('')
+        return this.db.promise().query('INSERT INTO department (name) VALUE (?)')
     }
 
     createRole() {
-        return this.db.promise().query('')
+        return this.db.promise().query('INSERT INTO role (title, salary, department_id)VALUES (?,?,?')
     }
 
     createEmployee() {
-        return this.db.promise().query('')
+        return this.db.promise().query('INSERT INTO candidates (first_name, last_name, role_id, manager_id)VALUES (?,?,?,?')
     }
 
     updateEmployeeRole() {
-        return this.db.promise().query('')
+        return this.db.promise().query('UPDATE employee SET employee.role_id = ? WHERE role_id = ?')
     }
 }
 
